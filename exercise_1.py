6efc02cd8fde88e48ffa28b6d992ec241e0115b5
Brian Burnstein <brianburnstein@0587348514.wireless.umich.net>
2019-11-22 17:57:39 -0500

    Added Brian

--- exercise_1.py
+++ exercise_1.py
@@ -1,10 +1,10 @@
 MEMBER_1 = "Drew"
 MEMBER_2 = "Ayaz"
-MEMBER_3 = "Ramon"
+MEMBER_3 = "Brian"
 
 MEMBER_1_HOME = "Ann Arbor, MI"
 MEMBER_2_HOME = "Baku, Azerbaijan"
-MEMBER_3_HOME = "Monterrey, Mexico"
+MEMBER_3_HOME = "Farmington Hills, Michigan"
 
 MEMBERS = {
     MEMBER_1:MEMBER_1_HOME,
